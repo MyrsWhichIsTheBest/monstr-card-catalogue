77e9ff42b05e80830ba905be40410d90be52a21e
Tim <nat@middleton.school.nz>
2023-05-16 13:47:41 +1200

    he

--- Add Card Function v8.py
+++ Add Card Function v8.py
@@ -6,18 +6,31 @@ This is my trialling for add card function
 import easygui
 
 
-def add_card(name):
-    stats = {}
-    for stat_name in ["Strength", "Speed", "Stealth", "Cunning"]:
+def stats_format(dictionary, message=""):
+    formatted_string = message
+    for key, value in dictionary.items():
+        formatted_string += f"\n {key}: {value}"
+    return formatted_string
+
+
+def add_card(name, new_stats):
+    for stat_name in list(new_stats.keys()):
         while True:
-            value = input(f"Enter the {stat_name} stat (1-25): ")
-            if value.isdigit() and 1 <= int(value) <= 25:
-                stats[stat_name] = int(value)
+            value = easygui.integerbox(f"Enter the {stat_name} stat (1-25): ")
+            if 1 <= value <= 25:
+                new_stats[stat_name] = value
                 break
             else:
-                print("Please enter a valid integer between 1 and 25.")
+                easygui.msgbox("Please only input whole numbers", ok_button="Ugh Fine...")
+                # error message
+    return [name, new_stats]
 
-    return {name: list(stats.values())}
 
+template = {"Strength": 0, "Speed": 0, "Stealth": 0, "Cunning": 0}
+output = add_card(easygui.enterbox("What is the name of the new card?"), template)
+# format dictionary to print out
+# catalogue.update({output[0]: list(output[1].values())})
+# this code above adds the output to the catalogue (aka monster_card_dictionary v1)
+easygui.msgbox(stats_format(output[1], f"You successfully created a New Monster Card: {output[0]}"))
+# output is the return of the add_card() function which returns the stats and the name
 
-print(add_card(easygui.enterbox("What name?")))
